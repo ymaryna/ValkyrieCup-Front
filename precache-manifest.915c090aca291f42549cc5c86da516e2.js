eeb9d608e0e5363e78635c03f715264b9a92ba6e
Marina Cid Reyes <ymaryna@MBP-de-Marina.technicolor.net>
2020-03-05 06:28:13 +0100

    Updates

---
+++ precache-manifest.915c090aca291f42549cc5c86da516e2.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7cf6e2bcae61496b0703a303e9b7e1",
+    "url": "/ValkyrieCup-Front/index.html"
+  },
+  {
+    "revision": "b1e67ac440fb6249a5ea",
+    "url": "/ValkyrieCup-Front/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "94362eda79444ba39c49",
+    "url": "/ValkyrieCup-Front/static/css/main.2537b4b8.chunk.css"
+  },
+  {
+    "revision": "b1e67ac440fb6249a5ea",
+    "url": "/ValkyrieCup-Front/static/js/2.9ef2ed9e.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/ValkyrieCup-Front/static/js/2.9ef2ed9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94362eda79444ba39c49",
+    "url": "/ValkyrieCup-Front/static/js/main.a2c1428a.chunk.js"
+  },
+  {
+    "revision": "0979c2bdd6ed76037382",
+    "url": "/ValkyrieCup-Front/static/js/runtime-main.6d9b7570.js"
+  },
+  {
+    "revision": "7a24cf5a2d07d06f832eee4318f8728b",
+    "url": "/ValkyrieCup-Front/static/media/10.7a24cf5a.png"
+  },
+  {
+    "revision": "b7db46afca9cf945d910d164fe26f3ca",
+    "url": "/ValkyrieCup-Front/static/media/11.b7db46af.png"
+  },
+  {
+    "revision": "dc4020b90b2d69e49859146ad88ef4a2",
+    "url": "/ValkyrieCup-Front/static/media/12.dc4020b9.png"
+  },
+  {
+    "revision": "5e725ef3e8c0eecf826b9526b687f8ff",
+    "url": "/ValkyrieCup-Front/static/media/13.5e725ef3.png"
+  },
+  {
+    "revision": "371dc47e5236d3c1be7f362b2ecd5812",
+    "url": "/ValkyrieCup-Front/static/media/14.371dc47e.png"
+  },
+  {
+    "revision": "b4156acf6f9e01c6c21047e75b3f7692",
+    "url": "/ValkyrieCup-Front/static/media/15.b4156acf.png"
+  },
+  {
+    "revision": "e2421467ae77c1b6fc9a26cedb851835",
+    "url": "/ValkyrieCup-Front/static/media/16.e2421467.png"
+  },
+  {
+    "revision": "6cf695ceda8d27278e4f5ee8c968ed43",
+    "url": "/ValkyrieCup-Front/static/media/17.6cf695ce.png"
+  },
+  {
+    "revision": "1deb83d9428a75d25635965a85d7cdbe",
+    "url": "/ValkyrieCup-Front/static/media/18.1deb83d9.png"
+  },
+  {
+    "revision": "74bd4575c85380879d5d44628fb5d4b1",
+    "url": "/ValkyrieCup-Front/static/media/19.74bd4575.png"
+  },
+  {
+    "revision": "ddb9e58c9126f9bde41af59cb1b8498d",
+    "url": "/ValkyrieCup-Front/static/media/1_AER RGB.ddb9e58c.jpg"
+  },
+  {
+    "revision": "e7f67683974b88cbfdeadcc5e9a6d8d2",
+    "url": "/ValkyrieCup-Front/static/media/20.e7f67683.png"
+  },
+  {
+    "revision": "63792b19bfca772ca346a1660a715ba2",
+    "url": "/ValkyrieCup-Front/static/media/21.63792b19.png"
+  },
+  {
+    "revision": "d9437ecf796c8ea7c79ea42c8363ef6f",
+    "url": "/ValkyrieCup-Front/static/media/4.d9437ecf.png"
+  },
+  {
+    "revision": "49f94bef6a5ceb50ea60db06a3c09d29",
+    "url": "/ValkyrieCup-Front/static/media/5.49f94bef.png"
+  },
+  {
+    "revision": "ea98a68653376031f83ebf4632e3c90b",
+    "url": "/ValkyrieCup-Front/static/media/6.ea98a686.png"
+  },
+  {
+    "revision": "8a364583c807bfb510452352363e2133",
+    "url": "/ValkyrieCup-Front/static/media/7.8a364583.png"
+  },
+  {
+    "revision": "98a24a10002c4b986577e81d1ad1a86e",
+    "url": "/ValkyrieCup-Front/static/media/8.98a24a10.png"
+  },
+  {
+    "revision": "0603e7a997313298f8cd6c4ea2ab6119",
+    "url": "/ValkyrieCup-Front/static/media/9.0603e7a9.png"
+  },
+  {
+    "revision": "e55b2da346759f0837078b9b7b8185f8",
+    "url": "/ValkyrieCup-Front/static/media/Bracket-bg.e55b2da3.png"
+  },
+  {
+    "revision": "312fccbf7fc69496b9e0d440f7ba871a",
+    "url": "/ValkyrieCup-Front/static/media/CASTERS_ABICHU_SCYTHE.312fccbf.jpg"
+  },
+  {
+    "revision": "089f68a11b73e21a4b0fc9eb2dab963f",
+    "url": "/ValkyrieCup-Front/static/media/VALKYRIE CUP DEFAULT.089f68a1.jpg"
+  },
+  {
+    "revision": "fd0d0ae3d6685e7ae3a9d392f665420d",
+    "url": "/ValkyrieCup-Front/static/media/bg.fd0d0ae3.png"
+  },
+  {
+    "revision": "4bb0191d4a258b3d98ec9609ec7a6e5d",
+    "url": "/ValkyrieCup-Front/static/media/logo-r6.4bb0191d.png"
+  },
+  {
+    "revision": "ce05afdc3f3d0c405f812f126bf009e7",
+    "url": "/ValkyrieCup-Front/static/media/logo-vc-blanco.ce05afdc.png"
+  },
+  {
+    "revision": "cb895a82937406838c39e5ba0676a4e1",
+    "url": "/ValkyrieCup-Front/static/media/tom-clancy-s-rainbow-six-siege_3840x2160.cb895a82.jpg"
+  },
+  {
+    "revision": "54e55479844de7c9a17fd22e899529f9",
+    "url": "/ValkyrieCup-Front/static/media/tom-clancy-s-rainbow-six-siege_bg.54e55479.jpg"
+  },
+  {
+    "revision": "d73a81373dfe612fe4c79e08cb841fe2",
+    "url": "/ValkyrieCup-Front/static/media/tom-clancy-s-rainbow-six-siege_bg_03.d73a8137.jpg"
+  },
+  {
+    "revision": "22e5889fee327262b64f073d8e1ff520",
+    "url": "/ValkyrieCup-Front/static/media/tom-clancy-s-rainbow-six-siege_bg_04.22e5889f.jpg"
+  },
+  {
+    "revision": "181b1c6a5a62ddcaa51a1f997b759a79",
+    "url": "/ValkyrieCup-Front/static/media/valkyriecup-video.181b1c6a.mp4"
+  }
+]);
